50c3a84aecc07a391a623007753d2d1a42013239
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-06-27 17:35:04 -0400

    savepoint

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyClcUUWciRdqB5IhC3yDyqwgdx9m1syjAY",
+    authDomain: "rps-multiplayer-3a589.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-3a589.firebaseio.com",
+    projectId: "rps-multiplayer-3a589",
+    storageBucket: "",
+    messagingSenderId: "100230497924"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+})
+
